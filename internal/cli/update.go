fb7b3ae864fcbea1fdf28c29cc4f36dd1cd881c5
Dante Dev <jdap.dmc@gmail.com>
2025-04-12 20:58:57 -0400

    [DOC]: comment update cmd

--- internal/cli/update.go
+++ internal/cli/update.go
@@ -5,6 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// UpdateCmd adds the "set" command to the CLI, allowing users to configure templates.
 func (cli *Base) UpdateCmd() {
 	cli.Root.AddCommand(&cobra.Command{
 		Use:   "set [name template] --repo [repository url] --name [name template] --branch [branch] --tag [tag] --summary [summary] --description [description]",
